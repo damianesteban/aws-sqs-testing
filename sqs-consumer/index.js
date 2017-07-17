62def2b6ed5f52489f5b55b38fdd0bd9812b3e44
Damian Esteban <damian.esteban@gmail.com>
2017-07-16 21:54:07 -0400

    Refactor to use bbc package

--- sqs-consumer/index.js
+++ sqs-consumer/index.js
@@ -1,77 +1,23 @@
-const express = require('express');
-const app = express();
 const aws = require('aws-sdk');
-const uuid = requitr('node-uuid');
-const queueUrl = "https://sqs.us-east-1.amazonaws.com/330377842965/starter-queue";
+const Consumer = require('sqs-consumer');
+const queueUrl = "";
 
 // Load the AWS credentials
 aws.config.loadFromPath(__dirname + '/config.json');
 
 const sqs = new aws.SQS();
 
-function receive(cb) {
-    var params = {
-        QueueUrl: queueUrl,
-	    MaxNumberOfMessages: 1,
-        VisibilityTimeout: 120,
-	    WaitTimeSeconds: 10
-    };
+const app = Consumer.create({
+    queueUrl: 'https://sqs.us-east-1.amazonaws.com/330377842965/starter-queue',
+    handleMessage: (message, done) => {
+        console.log(message.Body);
+        done();
+    },
+    sqs: new AQS.SQS()
+})
 
-     sqs.receiveMessage(params, function(err, data) {
-        if(err) {
-            cb(err);
-        }
-        else {
-            if (data.Messages === undefined) {
-                cb(null, null);
-            } else {
-                cb(null, data.Messages[0]);
-            }
-        }
-    });
-}
+app.on('error', (err) => {
+    console.log(`Error: ${err.message}`)
+})
 
-function process(message, cb) {
-    const body = JSON.parse(message.Body);
-    console.log(`Message body: ${body}`)
-}
-
-function acknowledge(message, cb) {
-    const params = {
-        QueueUrl: queueUrl,
-        ReceiptHandle: message.ReceiptHandle
-    };
-
-    sqs.deleteMessage(params, cb);
-}
-
-function run() {
-    receive((err, message) => {
-        if (err) {
-            throw err;
-        } else {
-            if (message === null) {
-                console.log('nothing to process...');
-                setTimeout(run, 1000);
-            } else {
-                console.log('Processing...')
-                process(message, (err) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        acknowledge(message, (err) => {
-                            if (err) {
-                                throw err;
-                            } else {
-                                console.log('Finished processing.')
-                                setTimeout(run, 1000);
-                            }
-                        })
-                    }
-                })
-            }
-        }
-    })
-}
-
-run();
+app.start();
